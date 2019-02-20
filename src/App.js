ecb3afad965bade67a1a4591f77ccaf833352844
aleksey leichenko <alekseyleychenko@gmail.com>
2019-02-20 15:22:10 +0200

    use state to set parameters

--- src/App.js
+++ src/App.js
@@ -4,20 +4,31 @@ import Car from './Car/Car';
 
 class App extends Component {
 
+    state = {
+      cars: [
+          {name: 'Ford', year: '2017'},
+          {name: 'Toyota', year: '2010'},
+          {name: 'Honda', year: '2019'}
+      ],
+        pageTitle: 'React Theory About "State"'
+    };
+
   render() {
       const divStyle = {
         // 'text-alegn' : 'center'
         textAlign: 'center'
       };
 
+      const cars = this.state.cars;
+
       return (
       <div className="App" style={divStyle}>
-        <h1 style={{'color': 'tomato', fontSize: '48px'}}>React theory</h1>
-         <Car name={'Ford'} year={'2017'}>
-          <p style={{color: 'blue'}}>Tag inside component though the props</p>
-         </Car>
-         <Car name={'Toyota'} year={'2010'}/>
-         <Car name="Honda" year="2019"/>
+          <h1 style={{color: 'tomato', fontSize: '48px', fontWeight: '700'}}>{this.state.pageTitle}</h1>
+
+         <Car name={cars[0].name} year={cars[0].year}/>
+         <Car name={cars[1].name} year={cars[1].year}/>
+         <Car name={cars[2].name} year={cars[2].year}/>
+
 
       </div>
     );
